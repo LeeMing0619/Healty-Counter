6971ed27c7935da3429b76f63f9f5f3bc5c4ddf0
LeeMing <48854224+LeeMing0619@users.noreply.github.com>
2021-12-14 05:14:18 -0800

    Clean completed

--- App.js
+++ App.js
@@ -11,25 +11,19 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  StatusBar,
   View,
-  Platform,
 } from 'react-native';
 import AppNavigator from './app/navigation/AppNavigator';
-import StatusBarView from './app/components/StatusBarView';
 import createStyles from './app/styles/style';
 
-const baseStyle = createStyles()
+const baseStyle = createStyles();
 
 const App = () => {
   return (
     <View style={baseStyle.container}>
-      {/* <StatusBarView /> */}
       <AppNavigator />
     </View>
   );
-}; 
+};
 
 export default App;
